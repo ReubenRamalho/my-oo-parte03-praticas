7c8f3b5f71a840c9bc51bdb32fcbdfafff54ac7f
ReubenRamalho <reubenramalho@gmail.com>
2022-11-04 19:35:18 -0300

    Update (add default case)

--- Prática 01. Agenda Simples/src/App.java
+++ Prática 01. Agenda Simples/src/App.java
@@ -22,78 +22,85 @@ public class App {
 
         for(int i = 0; i < ctrl; i++){
             
-            System.out.println("\n\n---Cadastro da pessoa " + (i+1) + "---");
-            System.out.printf("A pessoa a ser cadastrada é aluno ? (Y/N): ");
-            char resp = sc.next().charAt(0);
-            sc.nextLine();
+            System.out.println("\n\n---Cadastro da pessoa " + (i+1) + "---\n");
+            
+            while(true){
+                System.out.printf("A pessoa a ser cadastrada é aluno ? (Y/N): ");
+                char resp = sc.next().charAt(0);
+                sc.nextLine();
 
-            switch(resp){
+                switch(resp){
 
-                case 'y':
-                case 'Y':
-                    while(true){
-                        System.out.printf("Insira o nome: ");
-                        nome = sc.nextLine();
-                        
-                        System.out.printf("Insira o email: ");
-                        email = sc.nextLine();
+                    case 'y':
+                    case 'Y':
+                        while(true){
+                            System.out.printf("Insira o nome: ");
+                            nome = sc.nextLine();
+                            
+                            System.out.printf("Insira o email: ");
+                            email = sc.nextLine();
 
-                        System.out.printf("Insira a data de nascimento no formato (dd-MM-yyyy): ");
-                        try{
-                            data_str = sc.nextLine();
-                            data_nascimento = LocalDate.parse(data_str, DateTimeFormatter.ofPattern("dd-MM-yyyy"));
-                        }
-                        catch(Exception e){
-                            System.out.println("\nERRO 001: Data inválida.\nTente novamente.\n");
-                            continue;
-                        }
+                            System.out.printf("Insira a data de nascimento no formato (dd-MM-yyyy): ");
+                            try{
+                                data_str = sc.nextLine();
+                                data_nascimento = LocalDate.parse(data_str, DateTimeFormatter.ofPattern("dd-MM-yyyy"));
+                            }
+                            catch(Exception e){
+                                System.out.println("\nERRO 001: Data inválida.\nTente novamente.\n");
+                                continue;
+                            }
 
-                        System.out.printf("Insira a matricula: ");
-                        try{
-                            matricula = sc.next(".{5}");
-                        }
-                        catch (Exception e){
-                            System.out.println("\nERRO 002: O númedo de Matrícula deve conter apenas 5 digitos.\nTente novamente.\n");
-                            sc.nextLine();
-                            continue;
-                        }
+                            System.out.printf("Insira a matricula: ");
+                            try{
+                                matricula = sc.next(".{5}");
+                            }
+                            catch (Exception e){
+                                System.out.println("\nERRO 002: O númedo de Matrícula deve conter apenas 5 digitos.\nTente novamente.\n");
+                                sc.nextLine();
+                                continue;
+                            }
 
-                        if (!(matricula.matches("[0-9]+"))){
-                            System.out.println("\nERRO 003: O número da Matrícula deve conter apenas números.\nTente novamente.\n");
-                            sc.nextLine();
-                            continue;
-                        }                         
-                        else break;             
-                    }
-                    Aluno aluno = new Aluno(nome, email, data_nascimento, matricula);
-                    alunos.add(aluno);
-                    pessoas.add(aluno); 
-                    break;
-                    
-                case 'n':
-                case 'N':
-                    while(true){
-                        System.out.printf("Insira o nome: ");
-                        nome = sc.nextLine();
-                        
-                        System.out.printf("Insira o email: ");
-                        email = sc.nextLine();
-
-                        System.out.printf("Insira a data de nascimento no formato (dd-MM-yyyy): ");
-                        try{
-                            data_str = sc.nextLine();
-                            data_nascimento = LocalDate.parse(data_str, DateTimeFormatter.ofPattern("dd-MM-yyyy"));
+                            if (!(matricula.matches("[0-9]+"))){
+                                System.out.println("\nERRO 003: O número da Matrícula deve conter apenas números.\nTente novamente.\n");
+                                sc.nextLine();
+                                continue;
+                            }                         
+                            else break;             
                         }
-                        catch(Exception e){
-                            System.out.println("\nERRO 001: Data inválida.\nTente novamente.\n");
-                            continue;
-                        }
+                        Aluno aluno = new Aluno(nome, email, data_nascimento, matricula);
+                        alunos.add(aluno);
+                        pessoas.add(aluno); 
                         break;
-                    }
-                    Pessoa responsavel = new Pessoa(nome, email, data_nascimento);
-                    responsaveis.add(responsavel);
-                    pessoas.add(responsavel);
-                    break;
+                        
+                    case 'n':
+                    case 'N':
+                        while(true){
+                            System.out.printf("Insira o nome: ");
+                            nome = sc.nextLine();
+                            
+                            System.out.printf("Insira o email: ");
+                            email = sc.nextLine();
+
+                            System.out.printf("Insira a data de nascimento no formato (dd-MM-yyyy): ");
+                            try{
+                                data_str = sc.nextLine();
+                                data_nascimento = LocalDate.parse(data_str, DateTimeFormatter.ofPattern("dd-MM-yyyy"));
+                            }
+                            catch(Exception e){
+                                System.out.println("\nERRO 001: Data inválida.\nTente novamente.\n");
+                                continue;
+                            }
+                            break;
+                        }
+                        Pessoa responsavel = new Pessoa(nome, email, data_nascimento);
+                        responsaveis.add(responsavel);
+                        pessoas.add(responsavel);
+                        break;
+
+                    default:
+                        System.out.println("Digite uma resposta válida!\n");
+                        continue;    
+                }break;
             }
         }
     
@@ -105,5 +112,23 @@ public class App {
             
             else person.print_pessoa();
         }
+        while(true){
+            System.out.println(
+            "\n----------Menu----------\n" +
+            "1: Lista de Resposáveis\n" +
+            "2: Lista de Alunos\n" +
+            "3: Espeficicar contato\n" +
+            "0: Encerrar\n\n");
+            System.out.printf("Opção:");
+            int resp = sc.nextInt();
+
+            switch(resp){
+                case 1:
+
+
+
+
+            }
+        }    
     }
 }
